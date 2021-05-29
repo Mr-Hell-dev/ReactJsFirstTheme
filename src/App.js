5f7aa0fac1167b96caad426eea66a6220af572ba
Navdeep <navdeep422singh@gmail.com>
2021-05-29 21:22:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Switch, Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/" component />
+    </Switch>
+  );
+}
+
+export default App;
